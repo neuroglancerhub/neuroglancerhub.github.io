20aaa94ea119ec718d6f93a79dcd2d63d3d9dcbf
Jody Clements <jody@neomorphic.co.uk>
2019-06-17 16:11:45 -0400

    Removes gatsby branding

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Neuroglancer Hub`,
+    description: `Neuroglancer Hub is home to the neuroglancer react component and other annotation plugins`,
     author: `@gatsbyjs`,
   },
   plugins: [
