11ee45c583f5da97978aff61ff5e4895b4bb136c
Jody Clements <jody@neomorphic.co.uk>
2019-06-18 15:06:26 -0400

    Adds link to githubi org from footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 class Footer extends React.Component {
     render() {
         return (
             <section id="footer">
                 <ul className="icons">
+                  <li><a href="https://github.com/neuroglancerhub"><FontAwesomeIcon icon={faGithub} size="2x"/></a></li>
                 </ul>
                 <ul className="copyright">
                     <li>&copy; 2019 HHMI</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
